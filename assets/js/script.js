b61200ae92353917d077d14beb39468d993ca3a0
Andrew Howran <andrewhowrandev17@gmail.com>
2022-03-21 13:59:44 -0400

    added function to change bg colors after text input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,17 @@ $("#currentDay").text(CurrentDayVariable);
 
 //add a click listner on all the elements with the class of time-slot
 $(".row").on("click", ".time-slot", function () {
-  //get the text of the element clicked and trim it then store it in a variable
+  //get the text of the element clicked and trim it then store it in a variable, get the id too
   let text = $(this).text().trim();
-  //create a textarea element and add a class to it
+  let uniqueId = $(this).attr("id");
+
+  //create a textarea element and add a class and the specific id to it
   let textInput = $("<textarea>");
   textInput
-    .addClass(" text-field-input col-md-9 col-8 text-md-left")
-    .text(text);
+    .addClass("text-field-input col-md-9 col-8 text-md-left")
+    .text(text)
+    .attr("id", uniqueId);
+
   //replace the target of the event with the textarea element and trigger the focus state
   $(this).replaceWith(textInput);
   //auto focuses the textarea
@@ -24,68 +28,83 @@ $(".row").on("click", ".time-slot", function () {
 
 //on blur of a textarea with a parent of ".row"
 $(".row").on("blur", "textarea", function () {
+  //get the text and the id
   var text = $(this).val().trim();
+  let secondId = $(this).attr("id");
+
   var timeSlot = $("<div><div/>");
+  timeSlot.removeClass("past present future");
+  //call getbackgroundcolor to assign the right color dynamically
+  timeSlot = getBgColor(timeSlot, secondId);
   timeSlot
     .addClass("col-md-9 col-8 time-slot pt-3 text-md-left")
-    .text(text);
+    .text(text)
+    .attr("id", secondId);
+
   $(this).replaceWith(timeSlot);
 });
 
-//get a node list of all the time periods and give them a data-id from 9 to 17
-let listOfTimeBlocks = $(".task-to-do");
-for (i = 0; i < listOfTimeBlocks.length; i++) {
-  $(listOfTimeBlocks[i]).attr("data-id", i + 9);
-}
+function getBgColor(div, id) {
+  //declare an array to hold time of day
+  let timeOfDay = [
+    { eight: "8:00am" },
+    { nine: "9:00am" },
+    { ten: "10:00am" },
+    { eleven: "11:00am" },
+    { twelve: "12:00pm" },
+    { one: "1:00pm" },
+    { two: "2:00pm" },
+    { three: "3:00pm" },
+    { four: "4:00pm" },
+    { five: "5:00pm" },
+    { six: "6:00pm" },
+  ];
+  //correct way of indexing into a array of objects. NOT: timeOfDay[i].index
+  //console.log(timeOfDay[1][id])
 
-//delare time periods
-let timeOfDay = [
-  "9:00am",
-  "10:00am",
-  "11:00am",
-  "12:00pm",
-  "1:00pm",
-  "2:00pm",
-  "3:00pm",
-  "4:00pm",
-  "5:00pm",
-];
-
-//interval for every 20 minutes, goes through each time and checks to see if it is greater than or less than the current time
-setInterval(() => {}, 1000 * 60 * 20);
-
-timeOfDay.forEach((time) => {
-  time = moment(time, "h:mma");
-
-  if (moment() > time) {
-    switch (time.format("h:mma")) {
-      case "9:00am":
-        $(listOfTimeBlocks[0]).addClass("past");
-        break;
-      case "10:00am":
-        $(listOfTimeBlocks[1]).addClass("past");
-        break;
-      case "11:00am":
-        $(listOfTimeBlocks[2]).addClass("past");
-        break;
-      case "12:00pm":
-        $(listOfTimeBlocks[3]).addClass("past");
-        break;
-      case "1:00pm":
-        $(listOfTimeBlocks[4]).addClass("past");
-        break;
-      case "2:00pm":
-        $(listOfTimeBlocks[5]).addClass("past");
-        break;
-      case "3:00pm":
-        $(listOfTimeBlocks[6]).addClass("past");
-        break;
-      case "4:00pm":
-        $(listOfTimeBlocks[7]).addClass("past");
-        break;
-      case "5:00pm":
-        $(listOfTimeBlocks[8]).addClass("past");
-        break;
+  for (i = 0; i < timeOfDay.length; i++) {
+    if (timeOfDay[i][id]) {
+      let firstMoment = moment(timeOfDay[i][id], "h:mma");
+      let secondMoment = moment(timeOfDay[i][id], "h:mma").add(1, "hours");
+      
+      if (moment().isBetween(firstMoment, secondMoment)) {
+        div.addClass("present");
+        return div;
+      } else if (!firstMoment.isAfter(moment())) {
+        div.addClass("past");
+        return div;
+      } else {
+        div.addClass("future");
+        return div;
+      }
+      //  else if (firstMoment.isbetween(timeOfDay[i - 1][id], timeOfDay[i][id])) {
+      //   div.addClass("present");
+      //   return div;
+      // }
     }
   }
-});
+}
+
+//setInterval(() => {
+//   //declare an array to hold time of day
+//   let timeOfDay = [
+//     "9:00am",
+//     "10:00am",
+//     "11:00am",
+//     "12:00pm",
+//     "1:00pm",
+//     "2:00pm",
+//     "3:00pm",
+//     "4:00pm",
+//     "5:00pm",
+//   ];
+
+//   let secondMoment = moment();
+
+//   timeOfDay.forEach((time) => {
+//     let firstMoment = moment(time, "h:mma");
+//     if (firstMoment.isBefore(secondMoment)) {
+
+//     }
+//   });
+// }, 1000);
