dba5068de69f5b5c2f5212dcaadf880f3432f526
Andrew Howran <andrewhowrandev17@gmail.com>
2022-03-21 16:04:29 -0400

    added feature to update time sections colors automatically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var timeSlot;
+
 //set a variable to the current date
 var CurrentDayVariable = moment().format("dddd, MMMM Do");
 //set a interval is run every 12 hours to update the time
@@ -32,20 +34,66 @@ $(".row").on("blur", "textarea", function () {
   var text = $(this).val().trim();
   let secondId = $(this).attr("id");
 
-  var timeSlot = $("<div><div/>");
+  timeSlot = $("<div><div/>");
   timeSlot.removeClass("past present future");
-  //call getbackgroundcolor to assign the right color dynamically
+  //call getbackgroundcolor to assign the right color dynamically, then add bootstrap classes for styling
   timeSlot = getBgColor(timeSlot, secondId);
   timeSlot
     .addClass("col-md-9 col-8 time-slot pt-3 text-md-left")
     .text(text)
     .attr("id", secondId);
 
+  //replace the textarea with the new time lot div
   $(this).replaceWith(timeSlot);
 });
 
+//select all elements with the class of timeslot and call a loop on them
+$(".time-slot").each(function (index) {
+  //remove classes to ensure that no extra classes are applied
+  $(this).removeClass("past present future");
+  //delcare times of the day to reference
+  let timeOfDayInterval = [
+    { eight: "8:00am" },
+    { nine: "9:00am" },
+    { ten: "10:00am" },
+    { eleven: "11:00am" },
+    { twelve: "12:00pm" },
+    { one: "1:00pm" },
+    { two: "2:00pm" },
+    { three: "3:00pm" },
+    { four: "4:00pm" },
+    { five: "5:00pm" },
+    { six: "6:00pm" },
+  ];
+  //get the current elements id
+  let currentDivAttr = $(this).attr("id");
+  //loop through the dates array
+  for (i = 0; i < timeOfDayInterval.length; i++) {
+    //if the current elements id value matches the key of the days array check time
+    if (timeOfDayInterval[i][currentDivAttr]) {
+      //declare a variable as a moment object for the value of the matching time array and a variable thats one hour ahead
+      let firstMoment = moment(timeOfDayInterval[i][currentDivAttr], "h:mma");
+      let secondMoment = moment(
+        timeOfDayInterval[i][currentDivAttr],
+        "h:mma"
+      ).add(1, "hours");
+      //check if the times are either inbetween or after and apply the approprite color class, then return out of the loop
+      if (moment().isBetween(firstMoment, secondMoment)) {
+        $(this).addClass("present");
+        return;
+      } else if (!firstMoment.isAfter(moment())) {
+        $(this).addClass("past");
+        return;
+      } else {
+        $(this).addClass("future");
+        return;
+      }
+    }
+  }
+});
+
 function getBgColor(div, id) {
-  //declare an array to hold time of day
+  //declare an array of objects to hold time of day
   let timeOfDay = [
     { eight: "8:00am" },
     { nine: "9:00am" },
@@ -59,14 +107,13 @@ function getBgColor(div, id) {
     { five: "5:00pm" },
     { six: "6:00pm" },
   ];
-  //correct way of indexing into a array of objects. NOT: timeOfDay[i].index
-  //console.log(timeOfDay[1][id])
 
+  //NOTE: correct way of indexing into a array of objects is arrayName[index][key] NOT: arrayName[i].index
   for (i = 0; i < timeOfDay.length; i++) {
     if (timeOfDay[i][id]) {
       let firstMoment = moment(timeOfDay[i][id], "h:mma");
       let secondMoment = moment(timeOfDay[i][id], "h:mma").add(1, "hours");
-      
+
       if (moment().isBetween(firstMoment, secondMoment)) {
         div.addClass("present");
         return div;
@@ -77,34 +124,42 @@ function getBgColor(div, id) {
         div.addClass("future");
         return div;
       }
-      //  else if (firstMoment.isbetween(timeOfDay[i - 1][id], timeOfDay[i][id])) {
-      //   div.addClass("present");
-      //   return div;
-      // }
     }
   }
 }
 
-//setInterval(() => {
-//   //declare an array to hold time of day
-//   let timeOfDay = [
-//     "9:00am",
-//     "10:00am",
-//     "11:00am",
-//     "12:00pm",
-//     "1:00pm",
-//     "2:00pm",
-//     "3:00pm",
-//     "4:00pm",
-//     "5:00pm",
+//not sure if removing this will break the code
+// setInterval(() => {
+//   let timeOfDayArray = [
+//     { eight: "8:00am" },
+//     { nine: "9:00am" },
+//     { ten: "10:00am" },
+//     { eleven: "11:00am" },
+//     { twelve: "12:00pm" },
+//     { one: "1:00pm" },
+//     { two: "2:00pm" },
+//     { three: "3:00pm" },
+//     { four: "4:00pm" },
+//     { five: "5:00pm" },
+//     { six: "6:00pm" },
 //   ];
 
-//   let secondMoment = moment();
+//   //correct way of indexing into a array of objects. NOT: timeOfDay[i].index
+//   for (i = 0; i < timeOfDayArray.length; i++) {
+//     if (timeOfDay[i][id]) {
+//       let firstMoment = moment(timeOfDayArray[i][id], "h:mma");
+//       let secondMoment = moment(timeOfDayArray[i][id], "h:mma").add(1, "hours");
 
-//   timeOfDay.forEach((time) => {
-//     let firstMoment = moment(time, "h:mma");
-//     if (firstMoment.isBefore(secondMoment)) {
-
+//       if (moment().isBetween(firstMoment, secondMoment)) {
+//         div.addClass("present");
+//         return div;
+//       } else if (!firstMoment.isAfter(moment())) {
+//         div.addClass("past");
+//         return div;
+//       } else {
+//         div.addClass("future");
+//         return div;
+//       }
 //     }
-//   });
-// }, 1000);
+//   }
+// }, 1000 * 60 * 45);
